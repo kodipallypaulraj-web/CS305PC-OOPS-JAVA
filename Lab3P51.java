2227033df0646ce37019ebc93de9e27e27188373
kodipallypaulraj-web <kodipallypaulraj@gmail.com>
2025-09-17 19:41:49 +0530

    Add files via upload

---
+++ Lab3P51.java
@@ -0,0 +1,12 @@
+class Lab3P51{
+	String name;
+	public void demonstrate(){
+		System.out.println("iam method in class Lab3P51");
+	}
+	public void setName(String s){
+		this.name = s;
+	}
+	public String getName(){
+		return this.name;
+	}
+}
